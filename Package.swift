710f10dfffd7848e3d006335b37e3052243085a3
Anton Lobanov <alobanov11@gmail.com>
2022-04-09 16:27:03 +0300

    Remove tests

--- Package.swift
+++ Package.swift
@@ -17,6 +17,5 @@ let package = Package(
 		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
 		// Targets can depend on other targets in this package, and on products in packages this package depends on.
 		.target(name: "ReactSwift", dependencies: []),
-		.testTarget(name: "ReactSwiftTests", dependencies: ["ReactSwift"]),
 	]
 )
