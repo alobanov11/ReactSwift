4d27a58bd033e9c5564750f73e5163d13190f814
Anton Lobanov <alobanov11@gmail.com>
2022-05-08 19:52:12 +0300

    Add store error

--- Sources/Store/StoreError.swift
+++ Sources/Store/StoreError.swift
@@ -4,7 +4,16 @@
 
 import Foundation
 
-public enum StoreError {
+public enum StoreError: Error {
 	case error(Error)
 	case text(String)
+}
+
+extension StoreError: LocalizedError {
+	public var errorDescription: String? {
+		switch self {
+		case let .error(error): return error.localizedDescription
+		case let .text(text): return text
+		}
+	}
 }
