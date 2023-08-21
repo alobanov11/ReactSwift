5ddc7c2d90145cceca5d6f3fa9b447e192903b29
Anton Lobanov <alobanov11@gmail.com>
2023-08-22 00:44:38 +0300

    Add empty use case

--- Sources/UseCase.swift
+++ Sources/UseCase.swift
@@ -11,3 +11,17 @@ public protocol UseCase {
     var reducer: Reducer { get }
     var middleware: Middleware { get }
 }
+
+public struct EmptyUseCase<U: UseCase>: UseCase {
+    public typealias State = U.State
+    public typealias Action = U.Action
+    public typealias Effect = U.Effect
+
+    public let reducer: Reducer
+    public let middleware: Middleware
+
+    public init() {
+        self.reducer = { _, _ in }
+        self.middleware = { _, _ in .none }
+    }
+}
