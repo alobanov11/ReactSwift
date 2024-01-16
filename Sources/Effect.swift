2816fe4ce1c00d9820a895ae993326a7b0f73787
Anton Lobanov <alobanov11@gmail.com>
2024-01-16 17:43:39 +0300

    Improve

--- Sources/Effect.swift
+++ Sources/Effect.swift
@@ -83,4 +83,8 @@ extension Effect {
     public static func merge(_ tasks: Self...) -> Self {
         Self(operations: tasks.flatMap { $0.operations })
     }
+
+    public static func merge(_ tasks: [Self]) -> Self {
+        Self(operations: tasks.flatMap { $0.operations })
+    }
 }
